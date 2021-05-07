7c80a99482f317958dddea415638a61ca0692786
khh <visualkhh@gmail.com>
2021-05-07 19:55:45 +0900

    readme

--- examples/dev/src/index.ts
+++ examples/dev/src/index.ts
@@ -1,11 +1,9 @@
 import {SimpleApplication} from "simple-boot-front/SimpleApplication";
 import {AppRouter} from "./app/AppRouter";
 import {SimOption, UrlType} from "simple-boot-front/option/SimOption";
-import {SimProxyHandler} from "simple-boot-front/proxy/SimProxyHandler";
-import {Renderer} from "simple-boot-front/render/Renderer";
 import {Advice} from "./app/advice/Advice";
 
-const option = new SimOption(AppRouter).setAdvice(Advice);
+const option = new SimOption(AppRouter).setUrlType(UrlType.path).setAdvice(Advice);
 const simpleApplication = new SimpleApplication(option);
 simpleApplication.run();
 
