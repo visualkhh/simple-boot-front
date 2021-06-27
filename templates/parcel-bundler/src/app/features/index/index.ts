f102329659dfc9bd229d9101fe4f6068e21a3117
khh <visualkhh@gmail.com>
2021-06-27 15:23:49 +0900

    template

--- templates/parcel-bundler/src/app/features/index/index.ts
+++ templates/parcel-bundler/src/app/features/index/index.ts
@@ -8,125 +8,25 @@ import {Renderer} from "simple-boot-front/render/Renderer";
 import {SimstanceManager} from "simple-boot-front/simstance/SimstanceManager";
 import {Navigation} from "simple-boot-front/service/Navigation";
 import css from "./index.css";
-import {Title} from "./Title";
 import {EventListener} from "simple-boot-front/decorators/event/EventListener";
-import {SubTitle} from "./SubTitle";
+import {TestService} from "../../services/TestService";
 
 @Sim({scheme: 'index'})
 export class Index extends Module {
-    data = "default data";
-    thisData = 5151;
-    count = 0;
-    size = 10;
-
-    public title2 = new class extends Module {
-        constructor() {
-            super({value: '4444444'});
-        }
-    }();
-
-    public numbers = new class extends Module {
-        constructor() {
-            super({
-                template: `
-                                    <ul>
-                                    <!--%
-                                        for (let i of this.value) {
-                                            write('<li>' + i + '</li>');
-                                        }
-                                    %-->
-                                    </ul>
-                                    <button module-event-click="aa">aaa</button>
-                            `,
-                value: [1, 2, 3]
-            });
-        }
-        aa() {
-            console.log('number clikc => ', this.value);
-        }
-    }();
-
-    constructor(public title: Title, public v: ViewService, public manager: SimstanceManager, public navigation: Navigation) {
-        super({template: html, styleImports: [css], modules: {'cmodule': SubTitle}, name: 'index'});
+    data = "hello simple-boot-front";
+    randomData = 99;
+    randomColorData = '#000000';
+    constructor(public testService: TestService, public manager: SimstanceManager, public navigation: Navigation) {
+        super({template: html, styleImports: [css]});
     }
-
     onInit() {
     }
 
-    @EventListener({target: window, name: 'resize'})
-    callBackResize($event: Event, option: View<any>) {
-        console.log('---------', option, $event)
+    random() {
+        this.randomData = Math.floor(this.testService.calc(RandomUtils.random(0,55), RandomUtils.random(1,99)));
     }
 
-    @EventListener({target: '#zzz', name: 'input'})
-    callBackInput($event: Event, option: View<any>) {
-        console.log('----input-----', option, $event)
-    }
-
-    @PostConstruct
-    test(title: Title) {
-        // console.log('-->', title)
-    }
-
-    // public m = BTitle;
-    // createModule() {
-    //     return new BTitle();
-    // }
-
-    plusCount($event: KeyboardEvent, view: View<Element>) {
-        console.log('this.count--> ', this.count);
-        this.count++;
-    }
-    plusSize($event: KeyboardEvent, view: View<Element>) {
-        console.log('--->', this._refModule)
-        this.size++;
-    }
-    minusCount($event: KeyboardEvent, view: View<Element>) {
-        console.log('this.count--> ', this.count);
-        this.count--;
-    }
-
-    changeText($event: KeyboardEvent, view: View<Element>) {
-        // this.title.value = view.value;
-        this.title.datas.push(Math.floor(RandomUtils.random(1, 400)));
-        console.log('--->', this.title.datas)
-        // (this.title as ATitle).adata.name='zz'
-        // console.log('------->', this.title.value)
-        // console.log('------->', (SimGlobal.application?.simstanceManager as any)['_storage'])
-    }
-
-    inputChange($event: InputEvent, view: View<Element>) {
-        console.log('--inputChange----->', $event.data, view)
-    }
-
-    changeData() {
-        console.log('-->changeData')
-        this.numbers.value = [
-            Math.floor(RandomUtils.random(1, 400)),
-            Math.floor(RandomUtils.random(1, 400)),
-            Math.floor(RandomUtils.random(1, 400))
-        ];
-    }
-
-    public randomColor(): string {
-        return RandomUtils.getRandomColor();
-    }
-
-    thisDataChange() {
-        this.thisData = Math.floor(RandomUtils.random(1, 400));
-        console.log('thisDataChange', this.thisData)
-    }
-
-    go($event: KeyboardEvent, view: View<Element>) {
-        $event.preventDefault();
-    }
-
-    goto() {
-        console.log('-->na', this.navigation)
-        this.navigation.go('exception');
-    }
-
-    @PostConstruct
-    public post(renderer: Renderer) {
+    randomColor() {
+        this.randomColorData = RandomUtils.getRandomColor();
     }
 }
