daf0f057dc20c53dc18f8b66111359114da401ea
khh <visualkhh@gmail.com>
2021-07-03 22:36:04 +0900

    template parcel

--- templates/parcel-bundler/src/index.ts
+++ templates/parcel-bundler/src/index.ts
@@ -1,8 +1,7 @@
-import {AppRouter} from './app/AppRouter';
-import {Advice} from './app/advice/Advice';
-import {SimFrontOption, UrlType} from 'simple-boot-front/option/SimFrontOption';
 import {SimpleBootFront} from 'simple-boot-front/SimpleBootFront';
-
+import {Advice} from './app/advice/Advice';
+import {AppRouter} from './app/AppRouter';
+import {SimFrontOption, UrlType} from 'simple-boot-front/option/SimFrontOption';
 const option = new SimFrontOption(AppRouter, [Advice]).setUrlType(UrlType.path);
 const simpleApplication = new SimpleBootFront(option);
 simpleApplication.run();
