b5230c2ab1d52e6d4aae854f02d17dc8d2d74211
khh <visualkhh@gmail.com>
2021-08-26 16:49:30 +0900

    templates

--- examples/dev-onepage/src/app/features/index/index.ts
+++ examples/dev-onepage/src/app/features/index/index.ts
@@ -3,6 +3,7 @@ import { PostConstruct, Sim } from 'simple-boot-core/decorators/SimDecorator';
 import css from './index.css'
 import { User } from './User';
 import { Component } from 'simple-boot-front/decorators/Component';
+import { RandomUtils } from 'simple-boot-core/utils/random/RandomUtils';
 
 @Sim({scheme: 'index'})
 @Component({
@@ -15,12 +16,17 @@ export class Index {
     public cnt1 = 0;
     public cnt2 = 0;
     public test = {name: 'test', age:2}
+    public randomColorData = '#ff0000';
     onInit() {
         console.log('-->')
     }
 
     @PostConstruct
     public g(s: User) {
+    }
 
+    randomColor() {
+        console.log('------',this)
+        this.randomColorData = RandomUtils.getRandomColor();
     }
 }
